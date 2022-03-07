ec10c0117ab7ea3a3ef78b4d2e0ff44d63e51549
Florea George <florgeo_02@yahoo.com>
2022-03-07 21:07:29 +0200

    useReducer

--- 04-useEffect-project/src/App.js
+++ 04-useEffect-project/src/App.js
@@ -1,23 +1,29 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
-import Login from "./components/Login/Login";
-import Home from "./components/Home/Home";
-import MainHeader from "./components/MainHeader/MainHeader";
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-    const info = localStorage.getItem("isLoggedIn");
-    if (info === "1") setIsLoggedIn(true);
+    const storedUserLoggedInInformation = localStorage.getItem('isLoggedIn');
+
+    if (storedUserLoggedInInformation === '1') {
+      setIsLoggedIn(true);
+    }
   }, []);
+
   const loginHandler = (email, password) => {
-    localStorage.setItem("isLoggedIn", "1");
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem('isLoggedIn', '1');
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
-    localStorage.removeItem("isLoggedIn");
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
